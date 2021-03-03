4d3637004279d0c35695af255768113ccff3f57c
ajarchit <79588595+ajarchit@users.noreply.github.com>
2021-03-03 22:17:53 +0530

    Add files via upload

---
+++ random_quick.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std; 
+
+void swap(int* a, int* b) 
+{ 
+	int t= *a; 
+	*a=*b; 
+	*b=t; 
+} 
+int random(int arr[],int l,int r)
+{
+	int n=r-l+1;
+	int temp=rand()%n;
+	swap(&arr[l+temp],&arr[n]);
+}
+int partition(int arr[],int l,int h) 
+{ 
+	int pivot=arr[h]; 
+	int i=l-1; 
+
+	for (int j=l;j<h;j++) 
+	{ 
+		if (arr[j]<pivot) 
+		{ 
+			i++; 
+			swap(&arr[j],&arr[i]); 
+		} 
+	} 
+	swap(&arr[i+1],&arr[h]); 
+	return (i+1); 
+} 
+void quickSort(int arr[],int l,int h)
+{ 
+	if (l<h) 
+	{ 
+		int pivot = partition(arr,l,h); 
+		quickSort(arr,l,pivot-1); 
+		quickSort(arr,pivot+1,h); 
+	} 
+} 
+void print(int arr[], int n) 
+{  
+	for (int i=0;i<n;i++) 
+		cout <<arr[i]<< " "; 
+	cout << endl; 
+} 
+int main() 
+{ 
+	int arr[] = {10, 5, 8, 1, 6, 7}; 
+	int n = 6; 
+	quickSort(arr,0,n-1); 
+	cout << "Sorted array: \n"; 
+	print(arr, n); 
+	return 0; 
+}
